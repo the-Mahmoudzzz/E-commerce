2552cdc28b8e9fdf202972cbda2411d533b2c142
Mahmoud Mostafa <mhmmoud289@gmail.com>
2026-01-07 18:36:15 +0200

    Create Archticeture For Project add entity

---
+++ e-commerce.core/entities/User.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_commerce.core.entities
+{
+    public class User:IdentityUser<int>
+    {
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? DeletedAt { get; set; } 
+        public DateTime? UpdateddAt { get; set; } 
+        
+        
+    }
+}
