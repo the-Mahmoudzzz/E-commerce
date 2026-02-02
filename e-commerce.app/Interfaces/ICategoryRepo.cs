428f80313c592075dbafb1d6c5dda55a07295b5e
Mahmoud Mostafa <mhmmoud289@gmail.com>
2026-02-02 22:03:30 +0200

    Add category service and dto

---
+++ e-commerce.app/Interfaces/ICategoryRepo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_commerce.app.Interfaces
+{
+     interface ICategoryRepo
+    {
+        Task AddAsync(Category feedback);
+        Task<IEnumerable<Feedback>> GetAllAsync();
+        Task<IEnumerable<Feedback>> GetByTypeAsync(FeedbackType feedbackType);
+    }
+}
