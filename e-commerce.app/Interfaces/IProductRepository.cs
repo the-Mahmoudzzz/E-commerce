deb1a1784288327f8b2d7681d10571cba6d9a6ee
Mahmoud Alaa <mahmoudalaaaldien10@gmail.com>
2026-02-07 23:53:51 +0200

    add product feature

---
+++ e-commerce.app/Interfaces/IProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using e_commerce.core.entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_commerce.app.interfaces
+{
+    public interface IProductRepository
+    {
+        Task<Product?>GetByIdAsync(int id);
+        Task<IEnumerable<Product>> GetAllAsync();
+        Task<IEnumerable<Product>> GetByCategoryAsync(int CategoryId);
+        Task AddAsync(Product product);
+        Task UpdateAsync(Product product);
+        Task DeleteAsync(int id);
+        Task<IEnumerable<Product>> GetBySellerAsync(int sellerId);
+
+
+    }
+}
---
+++ e-commerce.app/Services/IServices/IProductService.cs
@@ -0,0 +1,22 @@
+﻿using e_commerce.app.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_commerce.app.servieses.iserviese
+{
+    public interface IProductService
+    {
+ 
+            Task<ProductDto> GetByIdAsync(int id);
+            Task<IEnumerable<summaryProductDto>> GetAllAsync();
+            Task<IEnumerable<summaryProductDto>> GetBySellerAsync(int sellerId);
+            Task AddProductAsync(CreateProductBySellerDto dto, int sellerId);
+            Task UpdateProductAsync(int id, UpdateProductBySellerDto dto);
+            Task ApproveProductAsync(int id, ApproveProductByAdminDto dto);
+            Task DeleteProductAsync(int id);
+      
+    }
+}
