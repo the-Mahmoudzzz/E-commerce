82590db03d4bc6f9cc92c415815a9b69f15f1b88
Mahmoud Mostafa <mhmmoud289@gmail.com>
2026-02-08 00:53:23 +0200

    fix table name

--- e-commerce.infra/Migrations/20260207224248_update-prodact-name.Designer.cs
+++ e-commerce.infra/Migrations/20260207224248_update-prodact-name.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using e_commerce.infra.Data;
 
@@ -11,9 +12,11 @@ using e_commerce.infra.Data;
 namespace e_commerce.infra.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260207224248_update-prodact-name")]
+    partial class updateprodactname
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ e-commerce.infra/Migrations/20260207224248_update-prodact-name.cs
@@ -0,0 +1,345 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace e_commerce.infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateprodactname : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_categories_categories_CategoryId",
+                table: "categories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_orderDetails_Product_ProductId",
+                table: "orderDetails");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Product_AspNetUsers_CreatedByAdminId",
+                table: "Product");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Product_AspNetUsers_SellerId",
+                table: "Product");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Product_AspNetUsers_UserId",
+                table: "Product");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Product_categories_CategoryId",
+                table: "Product");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductReviews_Product_ProductId",
+                table: "ProductReviews");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_shoppingCartItems_Product_ProductId",
+                table: "shoppingCartItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Wishlists_Product_ProductId",
+                table: "Wishlists");
+
+            migrationBuilder.DropIndex(
+                name: "IX_categories_CategoryId",
+                table: "categories");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Product",
+                table: "Product");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "categories");
+
+            migrationBuilder.RenameTable(
+                name: "Product",
+                newName: "Products");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Products",
+                newName: "ApprovedByAdminId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Product_UserId",
+                table: "Products",
+                newName: "IX_Products_ApprovedByAdminId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Product_SellerId",
+                table: "Products",
+                newName: "IX_Products_SellerId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Product_CreatedByAdminId",
+                table: "Products",
+                newName: "IX_Products_CreatedByAdminId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Product_CategoryId",
+                table: "Products",
+                newName: "IX_Products_CategoryId");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsApproved",
+                table: "Products",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Products",
+                table: "Products",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_categories_ParentCategoryId",
+                table: "categories",
+                column: "ParentCategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_categories_categories_ParentCategoryId",
+                table: "categories",
+                column: "ParentCategoryId",
+                principalTable: "categories",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_orderDetails_Products_ProductId",
+                table: "orderDetails",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductReviews_Products_ProductId",
+                table: "ProductReviews",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_AspNetUsers_ApprovedByAdminId",
+                table: "Products",
+                column: "ApprovedByAdminId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_AspNetUsers_CreatedByAdminId",
+                table: "Products",
+                column: "CreatedByAdminId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_AspNetUsers_SellerId",
+                table: "Products",
+                column: "SellerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_categories_CategoryId",
+                table: "Products",
+                column: "CategoryId",
+                principalTable: "categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_shoppingCartItems_Products_ProductId",
+                table: "shoppingCartItems",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Wishlists_Products_ProductId",
+                table: "Wishlists",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_categories_categories_ParentCategoryId",
+                table: "categories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_orderDetails_Products_ProductId",
+                table: "orderDetails");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductReviews_Products_ProductId",
+                table: "ProductReviews");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_AspNetUsers_ApprovedByAdminId",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_AspNetUsers_CreatedByAdminId",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_AspNetUsers_SellerId",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_categories_CategoryId",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_shoppingCartItems_Products_ProductId",
+                table: "shoppingCartItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Wishlists_Products_ProductId",
+                table: "Wishlists");
+
+            migrationBuilder.DropIndex(
+                name: "IX_categories_ParentCategoryId",
+                table: "categories");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Products",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "IsApproved",
+                table: "Products");
+
+            migrationBuilder.RenameTable(
+                name: "Products",
+                newName: "Product");
+
+            migrationBuilder.RenameColumn(
+                name: "ApprovedByAdminId",
+                table: "Product",
+                newName: "UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Products_SellerId",
+                table: "Product",
+                newName: "IX_Product_SellerId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Products_CreatedByAdminId",
+                table: "Product",
+                newName: "IX_Product_CreatedByAdminId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Products_CategoryId",
+                table: "Product",
+                newName: "IX_Product_CategoryId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Products_ApprovedByAdminId",
+                table: "Product",
+                newName: "IX_Product_UserId");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "categories",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Product",
+                table: "Product",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_categories_CategoryId",
+                table: "categories",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_categories_categories_CategoryId",
+                table: "categories",
+                column: "CategoryId",
+                principalTable: "categories",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_orderDetails_Product_ProductId",
+                table: "orderDetails",
+                column: "ProductId",
+                principalTable: "Product",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Product_AspNetUsers_CreatedByAdminId",
+                table: "Product",
+                column: "CreatedByAdminId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Product_AspNetUsers_SellerId",
+                table: "Product",
+                column: "SellerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Product_AspNetUsers_UserId",
+                table: "Product",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Product_categories_CategoryId",
+                table: "Product",
+                column: "CategoryId",
+                principalTable: "categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductReviews_Product_ProductId",
+                table: "ProductReviews",
+                column: "ProductId",
+                principalTable: "Product",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_shoppingCartItems_Product_ProductId",
+                table: "shoppingCartItems",
+                column: "ProductId",
+                principalTable: "Product",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Wishlists_Product_ProductId",
+                table: "Wishlists",
+                column: "ProductId",
+                principalTable: "Product",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
