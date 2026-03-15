241db7921878e617e6cf04b045bf43eac695fd7b
Mahmoud Mostafa <mhmmoud289@gmail.com>
2026-03-15 16:37:48 +0200

    Create Serviece Order

---
+++ e-commerce.app/Services/Implementation/OrderServiece.cs
@@ -0,0 +1,28 @@
+﻿using e_commerce.app.Dto.OrderDto;
+using e_commerce.app.Services.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_commerce.app.Services.Implementation
+{
+    public class OrderServiece : IOrderService
+    {
+        public Task CreateOrder(OrderCreateDto order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<OrderDTO>> GetOrderByCustomerId(int customeid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OrderDTO> GetOrderById(int orderid)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
