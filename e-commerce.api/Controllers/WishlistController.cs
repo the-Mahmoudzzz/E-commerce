25acc2cb7b4d1541466de06e8e8a0f4f292d9bf6
Mahmoud Alaa <mahmoudalaaaldien10@gmail.com>
2026-04-14 15:40:04 +0200

    Add Whishlist Feature

---
+++ e-commerce.api/Controllers/WishlistController.cs
@@ -0,0 +1,45 @@
+﻿using e_commerce.app.Dto.WishlistDto;
+using e_commerce.app.Services.IServices;
+using e_commerce.core.entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace e_commerce.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WishlistController : ControllerBase
+    {
+        private readonly IWishlistService wishlistService;
+
+        public WishlistController(IWishlistService wishlistService)
+        {
+            this.wishlistService = wishlistService;
+        }
+
+        [HttpGet("{userId}")]
+        public async Task<IActionResult> GetAll(int userId)
+        {
+            var items = await wishlistService.GetUserWishlistAsync(userId);
+            return Ok(items);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddToWishlistDto dto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            await wishlistService.AddToWishlistAsync(dto);
+            return Created();
+        }
+
+        [HttpDelete]
+        public IActionResult Remove(int userId, int productId)
+        {
+            wishlistService.RemoveFromWishlistAsync(userId, productId);
+            return Ok();
+        }
+    }
+}
---
+++ e-commerce.app/Mapping/WishlistProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using e_commerce.app.Dto.WishlistDto;
+using e_commerce.core.entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_commerce.app.Mapping
+{
+    public class WishlistProfile : Profile
+    {
+        public WishlistProfile()
+        {
+            CreateMap<Wishlist, WishlistDto>()
+                .ForMember(dest => dest.ProductName, opt => opt.MapFrom(src => src.Product.Name))
+                .ForMember(dest => dest.ProductPrice, opt => opt.MapFrom(src => src.Product.Price))
+                .ForMember(dest => dest.ProductImageUrl, opt => opt.MapFrom(src => src.Product.ImageUrl));
+
+            CreateMap<AddToWishlistDto, Wishlist>();
+        }
+    }
+}
