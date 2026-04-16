d7ca7dbeaebb58b04596dceb26210429145ff2b5
Mahmoud Diab <144825616+Hoda512@users.noreply.github.com>
2026-04-16 16:28:30 +0200

    Add_DockerFiles

--- e-commerce.api/Program.cs
+++ e-commerce.api/Program.cs
@@ -132,6 +132,11 @@ namespace e_commerce.api
 
 
             var app = builder.Build();
+            using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
