63c25546d51df6b90442acf0b388c7c62c6f65e1
Mahmoud Mostafa <mhmmoud289@gmail.com>
2026-04-19 17:41:15 +0200

    add notifcationevent to order,shipment,payment and fix userrole

--- e-commerce.app/External/NotificationHub.cs
+++ e-commerce.app/External/NotificationHub.cs
@@ -7,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace e_commerce.app.External
 {
-    public class NotificationHub:Hub
+    public  class NotificationHub:Hub
     {
+        public override async Task OnConnectedAsync()
+        {
+            // UserId بيجي من الـ JWT claim تلقائياً
+            await base.OnConnectedAsync();
+        }
     }
 }
