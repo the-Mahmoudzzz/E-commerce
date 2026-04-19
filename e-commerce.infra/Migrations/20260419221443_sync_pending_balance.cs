8a77767c1185e3512ea5a764049eb293057fa45b
Mahmoud Mostafa <mhmmoud289@gmail.com>
2026-04-20 00:16:35 +0200

    add isapproved in class user

---
+++ e-commerce.infra/Migrations/20260419221443_sync_pending_balance.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace e_commerce.infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class sync_pending_balance : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_AspNetUsers_CreatedByAdminId",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "LifeTimeEarnings",
+                table: "SellerWallets",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CreatedByAdminId",
+                table: "Products",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PaymentMethod",
+                table: "payments",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsApproved",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_AspNetUsers_CreatedByAdminId",
+                table: "Products",
+                column: "CreatedByAdminId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_AspNetUsers_CreatedByAdminId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "PendingBalance",
+                table: "SellerWallets");
+
+            migrationBuilder.DropColumn(
+                name: "Amount",
+                table: "payments");
+
+            migrationBuilder.DropColumn(
+                name: "IsApproved",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "LifeTimeEarnings",
+                table: "SellerWallets",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CreatedByAdminId",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PaymentMethod",
+                table: "payments",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_AspNetUsers_CreatedByAdminId",
+                table: "Products",
+                column: "CreatedByAdminId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
