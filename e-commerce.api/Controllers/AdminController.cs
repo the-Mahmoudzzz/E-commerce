4d49c0bb30994f91a6044e9b1c1e8f982c3ad9a9
Mahmoud Alaa <mahmoudalaaaldien10@gmail.com>
2026-04-21 02:28:23 +0200

    admin accept  the sellers

---
+++ e-commerce.api/Controllers/AdminController.cs
@@ -0,0 +1,52 @@
+﻿using e_commerce.core.entities;
+using e_commerce.core.Enum;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace e_commerce.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "Admin")]
+    public class AdminController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+
+        public AdminController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+
+        [HttpGet("pending-sellers")]
+        public async Task<IActionResult> GetPendingSellers()
+        {
+            var allSellers = await _userManager.
+                GetUsersInRoleAsync("Seller");
+
+
+            var pendingSellers = allSellers.
+                Where(u => !u.IsApproved)
+                .ToList();
+
+            return Ok(pendingSellers);
+        }
+
+        [HttpPost("approve-seller/{userId}")]
+        public async Task<IActionResult> ApproveSeller(int userId)
+        {
+            var user = await _userManager
+                .FindByIdAsync(userId.ToString());
+
+            if (user == null) 
+                return NotFound("Seller Not Found");
+
+            user.IsApproved = true;
+
+            await _userManager.UpdateAsync(user);
+
+            return Ok("The sealer was successfully approved.");
+        }
+    }
+}
